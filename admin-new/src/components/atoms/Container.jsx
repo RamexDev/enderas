2b6e9d2c9937fbd96f1d6ecb2c5da9f3416c6de5
Ramex <ramexworks@gmail.com>
2026-06-20 14:41:13 +0300

    deleted admin-new

--- admin-new/src/components/atoms/Container.jsx
+++
@@ -1,7 +0,0 @@
-export default function Container({ children, className = '', as: Tag = 'div', ...props }) {
-  return (
-    <Tag className={`mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8 ${className}`} {...props}>
-      {children}
-    </Tag>
-  )
-}
